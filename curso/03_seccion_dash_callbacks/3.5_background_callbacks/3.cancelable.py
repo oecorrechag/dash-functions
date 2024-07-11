02310851498d05bb7c708d836d1c4963b8ae9781
oecorrechag <oecorrechag@unal.edu.co>
2024-07-11 08:36:33 -0500

    Cancelable Callback

---
+++ curso/03_seccion_dash_callbacks/3.5_background_callbacks/3.cancelable.py
@@ -0,0 +1,44 @@
+import time
+import os
+
+from dash import Dash, DiskcacheManager, CeleryManager, Input, Output, html, callback
+
+if 'REDIS_URL' in os.environ:
+    # Use Redis & Celery if REDIS_URL set as an env variable
+    from celery import Celery
+    celery_app = Celery(__name__, broker=os.environ['REDIS_URL'], backend=os.environ['REDIS_URL'])
+    background_callback_manager = CeleryManager(celery_app)
+
+else:
+    # Diskcache for non-production apps when developing locally
+    import diskcache
+    cache = diskcache.Cache("./cache")
+    background_callback_manager = DiskcacheManager(cache)
+
+app = Dash(__name__, background_callback_manager=background_callback_manager)
+
+app.layout = html.Div(
+    [
+        html.Div([html.P(id="paragraph_id", children=["Button not clicked"])]),
+        html.Button(id="button_id", children="Run Job!"),
+        html.Button(id="cancel_button_id", children="Cancel Running Job!"),
+    ]
+)
+
+@callback(
+    output=Output("paragraph_id", "children"),
+    inputs=Input("button_id", "n_clicks"),
+    background=True,
+    running=[
+        (Output("button_id", "disabled"), True, False),
+        (Output("cancel_button_id", "disabled"), False, True),
+    ],
+    cancel=[Input("cancel_button_id", "n_clicks")],
+)
+def update_clicks(n_clicks):
+    # time.sleep(2.0)
+    return [f"Clicked {n_clicks} times"]
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
