96c0716d8ee3c7fd2353130e0e5987bd1c021f88
oecorrechag <oecorrechag@unal.edu.co>
2024-07-16 08:35:32 -0500

    Example 2: Disable Button While Callback Is Running

---
+++ curso/03_seccion_dash_callbacks/3.9_long_callbacks/2.disable.py
@@ -0,0 +1,35 @@
+import time
+import dash
+from dash import html, Input, Output
+from dash.long_callback import DiskcacheLongCallbackManager
+
+## Diskcache
+import diskcache
+
+cache = diskcache.Cache("./cache")
+long_callback_manager = DiskcacheLongCallbackManager(cache)
+
+app = dash.Dash(__name__, long_callback_manager=long_callback_manager)
+
+app.layout = html.Div(
+    [
+        html.Div([html.P(id="paragraph_id", children=["Button not clicked"])]),
+        html.Button(id="button_id", children="Run Job!"),
+    ]
+)
+
+@app.long_callback(
+    output=Output("paragraph_id", "children"),
+    inputs=Input("button_id", "n_clicks"),
+    running=[
+        (Output("button_id", "disabled"), True, False),
+    ],
+)
+def callback(n_clicks):
+    time.sleep(2.0)
+    return [f"Clicked {n_clicks} times"]
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
