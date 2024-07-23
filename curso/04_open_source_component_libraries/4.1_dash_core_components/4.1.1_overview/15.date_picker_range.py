4742bc52320700f8e1bb3a6709dc3929fb906d07
oecorrechag <oecorrechag@unal.edu.co>
2024-07-22 22:01:39 -0500

    DatePickerRange

---
+++ curso/04_open_source_component_libraries/4.1_dash_core_components/4.1.1_overview/15.date_picker_range.py
@@ -0,0 +1,15 @@
+from dash import Dash, dcc, html
+from datetime import date
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    dcc.DatePickerRange(
+        id='date-picker-range',
+        start_date=date(1997, 5, 3),
+        end_date_placeholder_text='Select a date!'
+    )
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
