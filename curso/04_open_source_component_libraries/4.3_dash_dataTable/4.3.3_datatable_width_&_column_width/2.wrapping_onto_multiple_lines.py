6226a4d3121d9288668125c30549ba0e824bea19
oecorrechag <oecorrechag@unal.edu.co>
2024-08-28 08:23:45 -0500

    Wrapping onto Multiple Lines

---
+++ curso/04_open_source_component_libraries/4.3_dash_dataTable/4.3.3_datatable_width_&_column_width/2.wrapping_onto_multiple_lines.py
@@ -0,0 +1,47 @@
+from dash import Dash, dash_table
+import pandas as pd
+from collections import OrderedDict
+
+data_election = OrderedDict(
+    [
+        (
+            "Date",
+            [
+                "July 12th, 2013 - July 25th, 2013",
+                "July 12th, 2013 - August 25th, 2013",
+                "July 12th, 2014 - August 25th, 2014",
+            ],
+        ),
+        (
+            "Election Polling Organization",
+            ["The New York Times", "Pew Research", "The Washington Post"],
+        ),
+        ("Rep", [1, -20, 3.512]),
+        ("Dem", [10, 20, 30]),
+        ("Ind", [2, 10924, 3912]),
+        (
+            "Region",
+            [
+                "Northern New York State to the Southern Appalachian Mountains",
+                "Canada",
+                "Southern Vermont",
+            ],
+        ),
+    ]
+)
+
+df = pd.DataFrame(data_election)
+
+app = Dash(__name__)
+
+app.layout = dash_table.DataTable(
+    style_data={
+        'whiteSpace': 'normal',
+        'height': 'auto',
+    },
+    data=df.to_dict('records'),
+    columns=[{'id': c, 'name': c} for c in df.columns]
+)
+
+if __name__ == '__main__':
+    app.run(debug=True)
