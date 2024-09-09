8ecf98e464666c4c78cac65c8426d01d197ce02f
oecorrechag <oecorrechag@unal.edu.co>
2024-09-09 08:15:41 -0500

    Striped Rows

---
+++ curso/04_open_source_component_libraries/4.3_dash_dataTable/4.3.4_styling_the_datatable/5.striped_rows.py
@@ -0,0 +1,47 @@
+from dash import Dash, dash_table
+import pandas as pd
+from collections import OrderedDict
+
+data = OrderedDict(
+    [
+        ("Date", ["2015-01-01", "2015-10-24", "2016-05-10", "2017-01-10", "2018-05-10", "2018-08-15"]),
+        ("Region", ["Montreal", "Toronto", "New York City", "Miami", "San Francisco", "London"]),
+        ("Temperature", [1, -20, 3.512, 4, 10423, -441.2]),
+        ("Humidity", [10, 20, 30, 40, 50, 60]),
+        ("Pressure", [2, 10924, 3912, -10, 3591.2, 15]),
+    ]
+)
+
+df = pd.DataFrame(data)
+
+app = Dash(__name__)
+
+app.layout = dash_table.DataTable(
+    data=df.to_dict('records'),
+    columns=[{'id': c, 'name': c} for c in df.columns],
+
+    style_cell_conditional=[
+        {
+            'if': {'column_id': c},
+            'textAlign': 'left'
+        } for c in ['Date', 'Region']
+    ],
+    style_data={
+        'color': 'black',
+        'backgroundColor': 'white'
+    },
+    style_data_conditional=[
+        {
+            'if': {'row_index': 'odd'},
+            'backgroundColor': 'rgb(220, 220, 220)',
+        }
+    ],
+    style_header={
+        'backgroundColor': 'rgb(210, 210, 210)',
+        'color': 'black',
+        'fontWeight': 'bold'
+    }
+)
+
+if __name__ == '__main__':
+    app.run(debug=True)
