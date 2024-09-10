2b5795e7adce9778fa21366c7b27f63bf920bb9d
oecorrechag <oecorrechag@unal.edu.co>
2024-09-10 08:49:56 -0500

    Styling Editable Columns

---
+++ curso/04_open_source_component_libraries/4.3_dash_dataTable/4.3.4_styling_the_datatable/10.styling_editable_columns.py
@@ -0,0 +1,38 @@
+from dash import Dash, dash_table
+import pandas as pd
+from collections import OrderedDict
+
+data = OrderedDict(
+    [
+        ("Date", ["2015-01-01", "2015-10-24", "2016-05-10", "2017-01-10", "2018-05-10", "2018-08-15"]),
+        ("Region", ["Montreal", "Toronto", "New York City", "Miami", "San Francisco", "London"]),
+        ("Temperature", [1, -20, 3.512, 4, 10423, -441.2]),
+        ("Humidity", [10, 20, 30, 40, 50, 60]),
+        ("Pressure", [2, 10924, 3912, -10, 3591.2, 15]),
+    ]
+)
+
+df = pd.DataFrame(data)
+
+app = Dash(__name__)
+
+app.layout = dash_table.DataTable(
+    data=df.to_dict('records'),
+    columns=[
+        {'id': c, 'name': c, 'editable': (c == 'Humidity')}
+        for c in df.columns
+    ],
+    style_data_conditional=[{
+        'if': {'column_editable': False},
+        'backgroundColor': 'rgb(30, 30, 30)',
+        'color': 'white'
+    }],
+    style_header_conditional=[{
+        'if': {'column_editable': False},
+        'backgroundColor': 'rgb(30, 30, 30)',
+        'color': 'white'
+    }],
+)
+
+if __name__ == '__main__':
+    app.run(debug=True)
