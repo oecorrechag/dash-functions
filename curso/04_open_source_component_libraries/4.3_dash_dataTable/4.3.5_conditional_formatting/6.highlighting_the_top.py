2de02a4e03e8ad75f220b2b38bc450078d4ff019
oecorrechag <oecorrechag@unal.edu.co>
2024-09-12 08:01:06 -0500

    Highlighting the Top Three or Bottom Three Values in a Column

---
+++ curso/04_open_source_component_libraries/4.3_dash_dataTable/4.3.5_conditional_formatting/6.highlighting_the_top.py
@@ -0,0 +1,51 @@
+from dash import Dash, dash_table
+import pandas as pd
+from collections import OrderedDict
+
+data = OrderedDict(
+    [
+        ("Date", ["2015-01-01", "2015-10-24", "2016-05-10", "2017-01-10", "2018-05-10", "2018-08-15"]),
+        ("Region", ["Montreal", "Toronto", "New York City", "Miami", "San Francisco", "London"]),
+        ("Temperature", [1, -20, 3.512, 4, 10423, -441.2]),
+        ("Humidity", [10, 20, 30, 40, 50, 60]),
+        ("Pressure", [2, 10924, 3912, -10, 3591.2, 15]),
+    ]
+)
+
+df = pd.DataFrame(data)
+
+app = Dash(__name__)
+
+app.layout = dash_table.DataTable(
+    data=df.to_dict('records'),
+    columns=[
+        {"name": i, "id": i} for i in df.columns
+    ],
+    style_data_conditional=(
+        [
+            {
+                'if': {
+                    'filter_query': '{{Temperature}} = {}'.format(i),
+                    'column_id': 'Temperature',
+                },
+                'backgroundColor': '#0074D9',
+                'color': 'white'
+            }
+            for i in df['Temperature'].nlargest(3)
+        ] +
+        [
+            {
+                'if': {
+                    'filter_query': '{{Pressure}} = {}'.format(i),
+                    'column_id': 'Pressure',
+                },
+                'backgroundColor': '#7FDBFF',
+                'color': 'white'
+            }
+            for i in df['Pressure'].nsmallest(3)
+        ]
+    )
+)
+
+if __name__ == '__main__':
+    app.run(debug=True)
