62d7673ec7093f2621d6b47a9f22a79388af1404
oecorrechag <oecorrechag@unal.edu.co>
2024-09-16 09:36:03 -0500

    Displaying Special Values for NaN or None Values

---
+++ curso/04_open_source_component_libraries/4.3_dash_dataTable/4.3.5_conditional_formatting/18.special_values_for_nan_or_none_values.py
@@ -0,0 +1,38 @@
+from dash import Dash, dash_table, html
+import pandas as pd
+import numpy as np
+from dash.dash_table.Format import Format
+
+data_with_none = [
+    {'Firm': 'Acme', '2017': '', '2018': 5, '2019': 10, '2020': 4},
+    {'Firm': 'Olive', '2017': None, '2018': 3, '2019': 13, '2020': 3},
+    {'Firm': 'Barnwood', '2017': np.NaN, '2018': 7, '2019': 3, '2020': 6},
+    {'Firm': 'Henrietta', '2017': 14, '2018': 1, '2019': 13, '2020': 1},
+]
+df = pd.DataFrame(data_with_none)
+
+df = df.fillna('N/A').replace('', 'N/A')
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.Pre(repr(df)),
+    dash_table.DataTable(
+        data=df.to_dict('records'),
+        columns=[{'name': i, 'id': i} for i in df.columns],
+        editable=True,
+        style_data_conditional=[
+            {
+                'if': {
+                    'filter_query': '{{{col}}} = "N/A"'.format(col=col),
+                    'column_id': col
+                },
+                'backgroundColor': 'tomato',
+                'color': 'white'
+            } for col in df.columns
+        ]
+    )
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
